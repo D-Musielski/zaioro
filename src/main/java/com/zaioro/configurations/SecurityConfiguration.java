76e9c32e388708225c8bbe735c22c7797790f7c3
Dominik Musielski <195023@edu.p.lodz.pl>
2017-05-07 13:00:10 +0200

    dz

--- src/main/java/com/zaioro/configurations/SecurityConfiguration.java
+++ src/main/java/com/zaioro/configurations/SecurityConfiguration.java
@@ -11,7 +11,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     private AuthenticationProvider authenticationProvider;
-
+    //gwno kurwa
 //    @Autowired
 //    @Qualifier("daoAuthenticationProvider")
 //    public void setAuthenticationProvider(AuthenticationProvider authenticationProvider) {
